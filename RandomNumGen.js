e0fc9781eb106304c7f2205b74d88177635650d6
Dan Alvarez <danalvarez5280@gmail.com>
2017-05-11 08:47:04 -0600

    My num gen game

--- RandomNumGen.js
+++ RandomNumGen.js
@@ -1,7 +1,9 @@
 // Phase One
 var yourGuess = document.getElementById('entry');
 
-var erase =document.getElementById('clear')
+var eraseButton =document.getElementById('clear')
+
+var lastGuess = document.getElementById('your-number');
 
 var box = document.getElementById('number');
 
@@ -9,21 +11,34 @@ var hint = document.getElementById('clue');
 
 var submitButton = document.getElementById('guess');
 
+var startOverButton = document.getElementById('reset')
+
+var minNumTextBox = document.getElementById('min');
+
+var maxNumTextBox = document.getElementById('max');
+
+function getRandom() {
+  var maxNum = parseInt(maxNumTextBox.value);
+  var minNum = parseInt(minNumTextBox.value);
+  return Math.floor(Math.random() * (maxNum - minNum)) + minNum;
+}
+
 var magicNumber = getRandom();
 
-var lastGuess = document.getElementById('your-number');
-
 submitButton.addEventListener('click', function() {
-  var letsPlay = yourGuess.value;
+  var letsPlay = parseInt(yourGuess.value);
+  lastGuess.innerText = "Your last guess was";
   box.innerText = letsPlay;
   hint.innerText = "";
-  lastGuess.innerText = "Your last guess was"
-  // figure out how to determine if a string is a number.
-  // if(letsPlay == NaN) {
-  //   hint.innerText = "Come on Buddy!"
-  // }
 
-  if (letsPlay > magicNumber) {
+  var maxNum = parseInt(maxNumTextBox.value);
+  var minNum = parseInt(minNumTextBox.value);
+
+  if ((letsPlay < minNum) || (letsPlay > maxNum)){
+    box.innerText = "ERROR";
+    hint.innerText = "Guess is outside of parameters"
+  }
+  else if (letsPlay > magicNumber) {
     hint.innerText= 'That is Too High';
   }
   else if (letsPlay < magicNumber) {
@@ -31,13 +46,63 @@ submitButton.addEventListener('click', function() {
   }
   else if (letsPlay == magicNumber) {
     hint.innerText = 'BOOM!';
+    window.alert("You Win!!!")
   }
-
+  else {
+    window.alert("That is not a number, when SkyNet takes over this 'Earth' you will be first to be enslaved")
+    hint.innerText = "Come On Now";
+  }
+  if ((letsPlay < minNum) || (letsPlay > maxNum)){
+    box.innerText = "ERROR";
+    hint.innerText = "Guess is outside of parameters"
+  }
 })
 
-erase.addEventListener('click', function() {
+eraseButton.addEventListener('click', function() {
+  if (yourGuess.value = "") {
+    document.getElementById('clear').setAttribute('disabled', true)
+  }
   yourGuess.value = "";
 })
-function getRandom() {
-return Math.floor(Math.random() * (99)) +1;
+
+startOverButton.addEventListener('click', function() {
+  magicNumber = getRandom();
+  console.log("your Magic number is: " + magicNumber);
+  yourGuess.value = "";
+  box.innerText = "";
+  hint.innerText = "";
+  lastGuess.innerText = "";
+})
+
+yourGuess.addEventListener('input', function() {
+  var letsPlay = yourGuess.value;
+  if (letsPlay.length == 0) {
+    eraseButton.disabled = true;
+    eraseButton.classList.add('class-disabled');
+    startOverButton.disabled = true;
+    startOverButton.classList.add('class-disabled');
+
+  }
+  else {
+    eraseButton.disabled = false;
+    eraseButton.classList.remove('class-disabled');
+    startOverButton.disabled = false;
+    startOverButton.classList.remove('class-disabled');
+  }
+})
+
+function checkNewRange() {
+  var maxNum = parseInt(maxNumTextBox.value);
+  var minNum = parseInt(minNumTextBox.value);
+  if (minNum < maxNum){
+    magicNumber = getRandom();
+    submitButton.disabled = false;
+    submitButton.classList.remove('class-disabled')
+  }
+  else {
+    submitButton.disabled = true;
+    submitButton.classList.add('class-disabled')
+  }
 }
+minNumTextBox.addEventListener("input", checkNewRange)
+maxNumTextBox.addEventListener('input', checkNewRange)
